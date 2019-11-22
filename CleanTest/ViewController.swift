6ca8497d90dc9ed3cabcd00ef11afd6b9b14c07c
Vadim Yushchenko <vadim.yushchenko@gmail.com>
2019-11-22 16:56:44 +0200

    Initial Commit

---
+++ CleanTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanTest
+//
+//  Created by Vadim Yushchenko on 22.11.2019.
+//  Copyright © 2019 Vadim Yushchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
